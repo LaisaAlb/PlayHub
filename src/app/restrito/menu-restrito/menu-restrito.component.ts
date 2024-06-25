ba4bfbf29db88c750a0509bcfe0709aa1795ab22
LaisaAlb <128616494+LaisaAlb@users.noreply.github.com>
2024-06-24 23:35:41 -0300

    Finalizando...

--- src/app/restrito/menu-restrito/menu-restrito.component.ts
+++ src/app/restrito/menu-restrito/menu-restrito.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/login.service';
+import { ProdutoService } from 'src/app/produto.service';
 
 @Component({
   selector: 'app-menu-restrito',
@@ -7,4 +10,12 @@ import { Component } from '@angular/core';
 })
 export class MenuRestritoComponent {
 
+  constructor(private _produtoService:ProdutoService, private _router: Router, private _loginService: LoginService){}
+
+  logout(){
+    localStorage.clear();
+    this._loginService.setMostraMenu(true);
+    this._router.navigate(['/login']);
+  }
+  
 }
--- src/app/restrito/restrito-routing-modele.ts
+++ src/app/restrito/restrito-routing-modele.ts
@@ -4,14 +4,15 @@ import { RouterModule, Router, Routes } from "@angular/router";
 import { ListaProdutoComponent } from './lista-produto/lista-produto.component';
 import { AtualizarProdutoComponent } from './atualizar-produto/atualizar-produto.component';
 import { RestritoComponent } from './restrito.component';
+import { GuardGuard } from '../guard.guard';
 
 const restritoRoutes: Routes = [
     { 
         path: 'restrito', component: RestritoComponent, children: [
-        { path: 'cadastro', component: CadastroProdutoComponent}, 
-        {path: 'lista', component: ListaProdutoComponent}, 
-        { path: 'editar/:id', component: AtualizarProdutoComponent}
-    ]}, 
+            { path: 'cadastro', component: CadastroProdutoComponent, canActivate: [GuardGuard] },
+            { path: 'lista', component: ListaProdutoComponent, canActivate: [GuardGuard] },
+            { path: 'editar/:id', component: AtualizarProdutoComponent, canActivate: [GuardGuard] }
+        ]}, 
     {path: '', redirectTo: '/restrito/lista', pathMatch: 'full'}
 ]
 
